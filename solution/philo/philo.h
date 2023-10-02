c53c1fe2b4d4c2d189aaf9d98aae32888062f5ff
Daria Kolodzey <kolodaria@gmail.com>
2023-10-02 12:49:12 +0000

    dev: Create data structures

--- solution/philo/philo.h
+++ solution/philo/philo.h
@@ -6,24 +6,36 @@
 /*   By: dkolodze <dkolodze@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/09/29 15:21:31 by dkolodze      #+#    #+#                 */
-/*   Updated: 2023/09/29 15:53:41 by dkolodze      ########   odam.nl         */
+/*   Updated: 2023/10/02 12:48:21 by codespace     ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
-typedef struct s_simulation_parameters {
-	int	number_of_philosophers;
-	int	time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
-	int	number_of_times_each_philosopher_must_eat;
-}	t_simulation_parameters;
+# include <pthread.h>
 
-typedef struct s_simulation_state {
-	int	n_dead;
-	int	n_hungry;
-}	t_simulation_state;
+struct	s_philosopher;
+
+typedef struct s_simulation {
+	int						number_of_philosophers;
+	int						time_to_die;
+	int						time_to_eat;
+	int						time_to_sleep;
+	int						number_of_times_each_philosopher_must_eat;
+	struct s_philosopher	*philosophers;
+	pthread_mutex_t			*forks;
+	pthread_mutex_t			mutex;
+	int						is_running;
+}	t_simulation;
+
+typedef struct s_philosopher {
+	pthread_t		thread;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
+	int				eaten_meals;
+	int				latest_meal_start;
+	t_simulation	*simulation;
+}	t_philosopher;
 
 #endif
